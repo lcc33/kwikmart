bb7511428fb8a697a287764263465c4530edc16c
muhammad <bryanedwarding@gmail.com>
2025-05-26 02:23:34 +0100

    sell,saved and profile pages added

---
+++ auth/main.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1Tt1hCnaUxNbRxd4jwixR4kw8sux9gdE",
+  authDomain: "kwikmart-1e6ac.firebaseapp.com",
+  projectId: "kwikmart-1e6ac",
+  storageBucket: "kwikmart-1e6ac.firebasestorage.app",
+  messagingSenderId: "721070715106",
+  appId: "1:721070715106:web:949e935a8abeeca86b2ebb",
+  measurementId: "G-2NDQDEJ45N",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
